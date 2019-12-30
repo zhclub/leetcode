c75891ad38f5811fdd208da05b39c7f0d421baee
zhouhao <1076376276@qq.com>
2019-12-30 09:29:29 +0800

    1

---
+++ src/test/java/TwoSumTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+class TwoSumTest {
+
+    @Test
+    void test() {
+        TwoSum twoSum = new TwoSum();
+
+        Assertions.assertEquals(
+                "[0, 1]",
+                Arrays.toString(twoSum.twoSum(new int[]{2, 7, 11, 15}, 9))
+        );
+    }
+
+}
