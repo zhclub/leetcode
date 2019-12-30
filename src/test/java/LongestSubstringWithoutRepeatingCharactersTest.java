fcb9ef662f45fbcbd3ab01b278628485d4ca49a1
zhouhao <1076376276@qq.com>
2019-12-30 09:40:42 +0800

    3. 无重复字符的最长字串

---
+++ src/test/java/LongestSubstringWithoutRepeatingCharactersTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LongestSubstringWithoutRepeatingCharactersTest {
+
+    private LongestSubstringWithoutRepeatingCharacters characters = new LongestSubstringWithoutRepeatingCharacters();
+
+    @Test
+    void lengthOfLongestSubstring() {
+        assertEquals(3, characters.lengthOfLongestSubstring("abcabcbb"));
+        assertEquals(1, characters.lengthOfLongestSubstring("bbbbb"));
+        assertEquals(3, characters.lengthOfLongestSubstring("pwwkew"));
+    }
+}
