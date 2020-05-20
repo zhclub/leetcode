1a8e2dda0a3a6142c318a69b895f83ac495eb23a
zhouhao <1076376276@qq.com>
2020-05-20 19:29:06 +0800

    11. 盛最多水的容器

---
+++ src/test/java/ContainerWithMostWaterTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContainerWithMostWaterTest {
+
+    @Test
+    void maxArea() {
+        ContainerWithMostWater containerWithMostWater = new ContainerWithMostWater();
+
+        int i = containerWithMostWater.maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7});
+        assertEquals(i, 49);
+    }
+}
