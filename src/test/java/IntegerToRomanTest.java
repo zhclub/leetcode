56d2e34360ab79e7c9b8f1a6239c2140bf7ad4f3
zhouhao <1076376276@qq.com>
2020-05-23 11:41:00 +0800

    12. 整数转罗马数字

---
+++ src/test/java/IntegerToRomanTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IntegerToRomanTest {
+
+    @Test
+    void intToRoman() {
+        IntegerToRoman integerToRoman = new IntegerToRoman();
+
+        assertEquals("III", integerToRoman.intToRoman(3));
+        assertEquals("IV", integerToRoman.intToRoman(4));
+        assertEquals("IX", integerToRoman.intToRoman(9));
+        assertEquals("LVIII", integerToRoman.intToRoman(58));
+        assertEquals("MCMXCIV", integerToRoman.intToRoman(1994));
+
+    }
+}
