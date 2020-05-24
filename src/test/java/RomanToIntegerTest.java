3de8c5dc550e80b491b0bb2951c71be0aba4e824
zhouhao <1076376276@qq.com>
2020-05-24 16:09:00 +0800

    13. 罗马数字转整数

---
+++ src/test/java/RomanToIntegerTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RomanToIntegerTest {
+
+    @Test
+    void romanToInt() {
+        RomanToInteger romanToInteger = new RomanToInteger();
+
+        assertEquals(3, romanToInteger.romanToInt("III"));
+        assertEquals(4, romanToInteger.romanToInt("IV"));
+        assertEquals(9, romanToInteger.romanToInt("IX"));
+        assertEquals(58, romanToInteger.romanToInt("LVIII"));
+        assertEquals(1994, romanToInteger.romanToInt("MCMXCIV"));
+    }
+}
